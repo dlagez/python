504a95726ce39f2dcd29ee0f903dcdb56965d976
roczhang <mr.roczhang@outlook.com>
2023-09-01 23:25:58 +0800

    测试了一些代码，但是都没成功

---
+++ 2023_08_20_pyg_cls/demo2_GAT.py
@@ -0,0 +1,18 @@
+from torch_geometric.nn import GATConv
+import torch
+
+
+class GAT(torch.nn.Module):
+    def __init__(self, in_feats, h_feats, out_feats):
+        super(GAT, self).__init__()
+        self.conv1 = GATConv(in_feats, h_feats, heads=8, concat=False)
+        self.conv2 = GATConv(h_feats, out_feats, heads=8, concat=False)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, training=self.training)
+        x = self.conv2(x, edge_index)
+
+        return x
