19e4a0f7e7ff9900436d9d2f559b1a3a590832c6
roczhang <mr.roczhang@outlook.com>
2024-12-27 16:39:00 +0800

    阿里接口测试

--- 2024-12-20-ai/qianwen/2024-12-23-chat/test-long.py
+++ 2024-12-20-ai/qianwen/2024-12-23-chat/test-long.py
@@ -7,7 +7,9 @@ client = OpenAI(
     base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
 )
 
-file_object = client.files.create(file=Path("data/ai/ht.pdf"), purpose="file-extract")
+file_object = client.files.create(file=Path("data/ai/发票-90123.2元.pdf"), purpose="file-extract")
 print(file_object.id)
 # test.pdf file-fe-DFOhTK7DfuzvAAcT5xITaNaY
 # ht.pdf file-fe-Ho3laSfryxwyMyuYSduWiaoO
+# 流程中台-软件定制开发合同.doc file-fe-XR8yF5tbyS6Uv668wIwrTpj1
+# 发票-90123.2元.pdf file-fe-o8IlfDuwfx9CkSCsuepuV0g3
--- 2024-12-20-ai/qianwen/2024-12-23-chat/test-long2.py
+++ 2024-12-20-ai/qianwen/2024-12-23-chat/test-long2.py
@@ -1,18 +1,19 @@
 import os
 from openai import OpenAI
+import time
 
 client = OpenAI(
     api_key=os.getenv("DASHSCOPE_API_KEY"),
     base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
 )
-
+start_time = time.time()
 completion = client.chat.completions.create(
     model="qwen-long",
     messages=[
         {'role': 'system', 'content': 'You are a helpful assistant.'},
         # {'role': 'system', 'content': 'fileid://file-fe-DFOhTK7DfuzvAAcT5xITaNaY'},
-        {'role': 'system', 'content': 'fileid://file-fe-Ho3laSfryxwyMyuYSduWiaoO'},
-        {'role': 'user', 'content': '我现在是审批人员，需要审批表单信息和合同内容是否匹配，表单数据：合同金额90万，合同内容为pdf file-fe-Ho3laSfryxwyMyuYSduWiaoO'}
+        {'role': 'system', 'content': f'fileid://file-fe-XR8yF5tbyS6Uv668wIwrTpj1,fileid://file-fe-o8IlfDuwfx9CkSCsuepuV0g3'},
+        {'role': 'user', 'content': '我现在是审批人员,需要审批合同信息和发票金额是否一致，此发票为合同的第5支付节点，一年免费运维期满后支付的金额，发票为：file-fe-o8IlfDuwfx9CkSCsuepuV0g3，合同内容为file-fe-XR8yF5tbyS6Uv668wIwrTpj1，检查发票和合同的异常。'}
     ],
     stream=True,
     stream_options={"include_usage": True}
@@ -25,3 +26,7 @@ for chunk in completion:
         print(chunk.model_dump())
 
 print({full_content})
+
+end_time = time.time()
+response_time = end_time - start_time
+print(f"Response Time: {response_time} seconds")
